b5a6e693b696feb6b27181ea5863213d1823a519
lisanlow <lisanlow@yahoo.com.sg>
2022-04-17 17:10:12 +0800

    Fixed up gpu002 and gpu003 samples

---
+++ gpu002/src/main.ts
@@ -0,0 +1,90 @@
+import $ from 'jquery';
+import { CheckWebGPU } from './helper';
+import { Shaders } from './shaders';
+
+const CreateTriangle = async (color='(1.0,1.0,1.0,1.0)') => {
+    const checkgpu = CheckWebGPU();
+    if(checkgpu.includes('Your current browser does not support WebGPU!')){
+        console.log(checkgpu);
+        throw('Your current browser does not support WebGPU!');
+    }
+
+    const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;        
+    const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;       
+    const device = await adapter?.requestDevice() as GPUDevice;
+    const context = canvas.getContext('webgpu') as unknown as GPUCanvasContext;
+    const format = 'bgra8unorm';
+
+    const devicePixelRatio = window.devicePixelRatio || 1;
+    const presentationSize = [
+        canvas.clientWidth * devicePixelRatio,
+        canvas.clientHeight * devicePixelRatio,
+    ];
+    const presentationFormat = context.getPreferredFormat(adapter);
+  
+    context.configure({
+      device,
+      format: presentationFormat,
+      size: presentationSize,
+    });
+
+    const shader = Shaders(color);
+    const pipeline = device.createRenderPipeline({
+        vertex: {
+            module: device.createShaderModule({                    
+                code: shader.vertex
+            }),
+            entryPoint: "main"
+        },
+        fragment: {
+            module: device.createShaderModule({                    
+                code: shader.fragment
+            }),
+            entryPoint: "main",
+            targets: [{
+                format: presentationFormat
+            }]
+        },
+        primitive:{
+           topology: "triangle-list",
+        }
+    });
+
+    function frame() {
+        // Sample is no longer the active page.
+        if (!canvas) return;
+    
+        const commandEncoder = device.createCommandEncoder();
+        const textureView = context.getCurrentTexture().createView();
+    
+        const renderPassDescriptor: GPURenderPassDescriptor = {
+          colorAttachments: [
+            {
+              view: textureView,
+              clearValue: { r: 0.0, g: 1.0, b: 0.0, a: 1.0 },
+              loadOp: 'clear',
+              storeOp: 'store',
+            },
+          ],
+        };
+    
+        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+        passEncoder.setPipeline(pipeline);
+        passEncoder.draw(3, 1, 0, 0);
+        passEncoder.end();
+    
+        device.queue.submit([commandEncoder.finish()]);
+        requestAnimationFrame(frame);
+    }
+    
+    requestAnimationFrame(frame);
+}
+
+CreateTriangle();
+$('#id-btn').on('click', ()=>{
+    const color = $('#id-color').val() as string;
+    CreateTriangle(color);
+});
+
+
+
